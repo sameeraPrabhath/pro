5b57600b5442838df68b29c7fe2d4a0cf86db55c
sameera_prabhath <sameeraprabhath.a@gmail.com>
2020-09-13 13:55:19 +0530

    COMMIT

--- app.js
+++ app.js
@@ -19,30 +19,74 @@ app.use(cors(corsOptions))
 
 
 
-function checkRank(url) {
-    const config = {
-        headers: {
-            "API-OPR": "40k4g8kww4s08kcw4oc4kowckkoooko88sk8wk44"
-        }
-    };
-    return axios.get('https://openpagerank.com/api/v1.0/getPageRank?domains[]=' + url, config)
-}
 
-function CheckInPhishtank(url) {
-    return client.query("SELECT * from phishtank WHERE url = $1", [url])
-}
+//////File read//////////////////////////
+var fs = require('fs');
+let queary = "INSERT INTO  alexa(url) VALUES ";
+var urlArray= [];
+async function readmyfile(){
+    try {
+        const data = await fs.readFileSync("top-1mtext.txt")
+        urlArray = data.toString().split('\r\n');
+      } catch (error) {
+        console.log(error)
+      }
 
-
-app.get("/all", async (req, res) => {
+      urlArray.forEach((element , i) => {
+          console.log(i)
+          queary =  queary + ` ('${element}') ,` ;  
+         
+      })
+    let modifyqueary = queary.substring(0, queary.length - 2) + `;`;
+    console.log(modifyqueary);
     client
-        .query("SELECT * from phishtank")
-        .then((result) => {
-            // console.log(result);
-            res.send(result.rows);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    .query(modifyqueary)
+    .then((result) => {
+      console.log(result);
+    //   res.send(result);  
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    console.log("result");
+}
+
+app.get("/filereadapi", async (req, res) => {  
+    readmyfile();
+  });
+////////////ENd file read /////////////////////
+
+
+//////////////////create table////////////////////////
+app.get("/tablecalex", async (req, res) => {  
+    client.query("CREATE TABLE alexa(url text)", (err, result) => {
+      console.log(err, result);
+      // pool.end();
+      res.send(result);
+    });
+  
+  });
+//////////////end create table ///////////////////////
+
+
+
+app.get("/api/searchinphish/:param", async (req, res) => {
+  console.log(req.params.param);
+  client
+      .query("SELECT * FROM phishtank WHERE url LIKE $1", ["%//"+req.params.param+"%"])
+      .then((result) => {
+          let found = result.rowCount ? true : false;
+          res.json({
+              query: req.params.param,
+              rows: result.rows,
+              rowCount: result.rowCount,
+              found: found,
+          });
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+
 });
 
 
@@ -50,7 +94,7 @@ app.get("/api/search/:param", async (req, res) => {
     console.log(req.params.param);
 
     client
-        .query("SELECT * from phishtank WHERE url = $1", [req.params.param])
+        .query("SELECT * from alexa WHERE url = $1", [req.params.param])
         .then((result) => {
             let found = result.rowCount ? true : false;
             res.json({
@@ -67,6 +111,83 @@ app.get("/api/search/:param", async (req, res) => {
 });
 
 
+
+app.get("/all", async (req, res) => {
+  client
+      .query("SELECT * from alexa")
+      .then((result) => {
+          console.log(result.rows.length);
+          res.send(result.rows);
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+});
+
+
+
+
+/////////OLD///////////////////////////////
+/////////////////////////////
+
+//insert data from firebase //////////////////////////////
+app.get("/insertbatch", async (req, res) => {
+  let queary = "INSERT INTO  phishtank(url) VALUES ";
+
+  axios
+    .get(
+      "https://test-udemy-fcbd0.firebaseio.com/domnames/-MF6HluQsn-czUhiyl7B/url.json"
+    )
+    .then((result) => {
+      console.log(result.data);
+
+      result.data.forEach((element) => {
+        console.log(element);
+        queary = queary + ` ('${element}') ,`;
+      });
+
+      console.log(queary.slice(0, 70));
+      console.log(queary.slice(queary.length - 30, queary.length));
+      let modifyqueary = queary.substring(0, queary.length - 2);
+      modifyqueary = modifyqueary + `;`;
+
+      console.log(
+        modifyqueary.slice(modifyqueary.length - 30, modifyqueary.length)
+      );
+
+      client
+        .query(modifyqueary)
+        .then((result) => {
+          console.log(result);
+          res.send(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+});
+///////////////////////////////////////
+////////////////////////////end insert form fire///////// /////////// 
+
+
+
+function checkRank(url) {
+  const config = {
+      headers: {
+          "API-OPR": "40k4g8kww4s08kcw4oc4kowckkoooko88sk8wk44"
+      }
+  };
+  return axios.get('https://openpagerank.com/api/v1.0/getPageRank?domains[]=' + url, config)
+}
+
+function CheckInPhishtank(url) {
+  return client.query("SELECT * from phishtank WHERE url = $1", [url])
+}
+
 app.post('/api/checkpish', function (req, resw) {
     var url = req.body.url;
     var finish = true;
@@ -74,6 +195,8 @@ app.post('/api/checkpish', function (req, resw) {
 
     checkRank(url).then((res) => {
         if (res.data.response[0].rank) {
+console.log(res.data.response[0]);
+
             resw.status(200).json({ status: 'ela' })
         }
         else {  
@@ -96,7 +219,7 @@ app.post('/api/checkpish', function (req, resw) {
     })
 })
 
-
+/////////////////////////end OLD ////////////////////
 
 
 
